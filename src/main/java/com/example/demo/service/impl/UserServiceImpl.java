9e0c4f43e0dbe97ffbbfdfd6e313e2055b1455a7
seraptemel <serap.temel@testinium.com>
2025-11-04 16:27:34 +0300

    add log

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -4,8 +4,10 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -13,11 +15,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save(User user) {
+        log.info("save user: {}", user.getName());
         userRepository.save(user);
     }
 
     @Override
     public User findByUsername(String username) {
+        log.info("findByUsername: {}", username);
         return userRepository.findByName(username);
     }
 }
